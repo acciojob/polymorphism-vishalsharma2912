f7def8e0be7a75a6ce29a83bcc4a5598f3a25aee
Vishal <vishaljind04@gmail.com>
2024-05-07 02:36:59 +0530

    commit polymorphy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y) {
+                     return x;
+        }
+        public int product(int x, int y, int z) {
+          return x;
+        }
+        public double product(double x, double y) {
+       return x;
+        }
+    }
+  public static void main (String[] args){
+      Product p = new Product();
+      p.product(1,2);
+      p.product(1,2,3);
+      p.product(3.14,7.8);
+  }
 }
